3e1c94387c9986c7b1928857f3baf2667c892b10
JonathanRSS1 <139492269+JonathanRSS1@users.noreply.github.com>
2023-10-10 23:36:45 -0600

    Add files via upload

---
+++ JS/Navbar y Footer/Footer/footerPages.js
@@ -0,0 +1,91 @@
+function generarFooterPages()
+{
+    const footerPages = `<div class="header1">
+
+    <!--Content before waves-->
+    <div class="inner-header flex">
+    <!--Just the logo.. Don't mind this-->
+    
+    <h1>SUPERMARKET STORAGE SYSTEM</h1>
+    </div>
+    
+    <p></p>
+    
+    <!--Waves Container-->
+    <div>
+    <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+    viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+    <defs>
+    <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+    </defs>
+    <g class="parallax">
+    <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(255,255,255,0.7" />
+    <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(255,255,255,0.5)" />
+    <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(255,255,255,0.3)" />
+    <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+    </g>
+    </svg>
+    </div>
+  
+    <footer class="bg-light text-center text-white">
+      <!-- Grid container -->
+      <div class="container p-4 pb-0">
+        <!-- Section: Social media -->
+        <section class="mb-4">
+        
+  </div>
+  
+  
+          <!-- Instagram -->
+          <a class="btn btn-outline-light btn-floating m-1"
+          style="background-color: #3b5998;"
+        href="https://instagram.com/supermarketsystem?igshid=MzRlODBiNWFlZA==" target="_blank"
+        role="button"
+            ><i class="bi bi-instagram"></i></a>
+    
+          
+          <a class="btn btn-outline-light btn-floating m-1" style="background-color: #3b5998;"
+          href="https://www.facebook.com/profile.php?id=61551852551516&mibextid=ZbWKwL" target="_blank"
+          role="button"
+            ><i class="bi bi-facebook"></i></a>
+    
+          <!-- Github -->
+          <a class="btn btn-outline-light btn-floating m-1" 
+          style="background-color: #3b5998;"
+        href="#!"
+        role="button"
+            ><i class="bi bi-messenger"></i>
+          </a>
+  
+          <a class="btn btn-outline-light btn-floating m-1" 
+          style="background-color: #3b5998;"
+        href="https://www.tiktok.com/@supermarketstorag?_t=8gGEQvdbii6&_r=1" target="_blank"
+        role="button"
+            ><i class="bi bi-tiktok"></i></a>
+  
+  
+          <a class="btn btn-outline-light btn-floating m-1" 
+          style="background-color: #3b5998;"
+          href="https://x.com/SSS_ITR?t=qcXe5wOqz1vQbEN2qnSxOQ&s=09" target="_blank"
+          role="button"
+          ><i class="bi bi-twitter"></i>
+          </a>
+  
+  
+          <a class="btn btn-outline-light btn-floating m-1"
+          style="background-color: #3b5998;"
+          href="../Pages/Contactanos.html" 
+          role="button"
+            ><i class="bi bi-envelope-at-fill"></i></a>
+    
+      <!-- Copyright -->
+      <div class="text-center p-3" style="background-color: #fff;">
+        <a class="text-black" href="#">© 2023 Copyright: SSS</a>
+        <a class="text-black" href="#">JJNMDA</a>
+      </div>
+      <!-- Copyright -->
+    </footer>
+  </div>`;
+
+  return footerPages;
+}
